cfd79b35e2d05c17d0613750512b577e16e6c0f8
Erik Dykema <erik@caserails.com>
2014-01-12 14:27:43 -0500

    Update CI Scripts

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 echo "Testing"
-pip install --use-wheel --find-links=dist/ django-angularui
+pip install --use-wheel --find-links=dist/ $PACKAGE_NAME
 echo "import angularui; print 'Hello World';" | python
