176f591e1f53c8a7d2f9bf2fff436fcdcf0ccd39
Erik <dykema@gmail.com>
2014-01-17 19:38:37 -0500

    testing

--- setup.py
+++ setup.py
@@ -1,48 +1,12 @@
-import os
-from os.path import join, dirname
 from setuptools import setup, find_packages
 
-
-def recursiveFileGenerator(rootDir):
-    """Recursively yield every file within the specified directory."""
-    for folder, subs, files in os.walk(rootDir):
-        for fileName in files:
-            dummy,dummy,result = os.path.join(folder,fileName).partition("/")
-            yield result
-
-
-def makeRecursiveFileList(rootDir):
-    """Return a list of every file within the specified directory."""
-    return list(recursiveFileGenerator(rootDir))
-
-
-try:
-    f = open(join(dirname(__file__), 'README.rst'))
-    long_description = f.read().strip()
-    f.close()
-except IOError:
-    long_description = None
-
 setup(
-    name='django-angularui',
+    name='django_angularui',
+    version='1.1.5',
+    packages=['django_angularui'],
+    include_package_data=True,
     zip_safe=False,
-    version='0.3',
-    url="---",
-    description='Angularui packaged in a handy django app to speed up new applications and reduce duplication.',
-    long_description=long_description,
-    author='---',
-    author_email='---',
-    keywords='django angular angularui staticfiles'.split(),
     platforms='any',
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Utilities',
-    ],
-    packages=['angularui'],
-    package_data={'angularui':makeRecursiveFileList("angularui")},
+    setup_requires=('ci_scripts'),
+    install_requires=('ci_scripts'),
 )
